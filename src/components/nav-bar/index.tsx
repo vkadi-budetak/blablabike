bd6e0957b66564419bee8d60163d1970cc1d4b3c
Vladyslav Kravchenko <alialiqtu@gmail.com>
2026-03-30 15:19:07 +0200

    fix: remove remaining merge conflict markers in navbar

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import {
   PATH_CATALOG,
   PATH_ABOUT_US,
-<<<<<<< HEAD
   USER_PROFILE,
   PATH_ADMIN,
 } from "@/app/path/path";
@@ -103,19 +102,6 @@ const NavBar = () => {
       </nav>
       {showSidebar && <Sidebar onClose={() => setShowSidebar(false)} />}
     </>
-=======
-  PATH_LOGIN,
-  PATH_SIGN_UP,
-} from "@/app/path/path";
-export default function NavBar() {
-  return (
-    <div>
-      <Link href={PATH_CATALOG}>Catalog</Link>
-      <Link href={PATH_ABOUT_US}>About Us</Link>
-      <Link href={PATH_LOGIN}>Login</Link>
-      <Link href={PATH_SIGN_UP}>Sign Up</Link>
-    </div>
->>>>>>> main
   );
 };
 
